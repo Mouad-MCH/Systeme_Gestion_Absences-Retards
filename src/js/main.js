91cd2529d6fe49ad3fbb95da67bad6ea7b864b0a
Mouad charadi <moadchradi12@gmail.com>
2025-12-17 03:32:58 +0100

    fix some problemes and add login html fils

--- src/js/main.js
+++ src/js/main.js
@@ -4,14 +4,18 @@
  * - DOM Variables
 \* --------------------- */
 
+let logeout = document.querySelector(".logeout");
 
+let btn_login = document.getElementById("btn_login");
+let username = document.getElementById("username");
+let password = document.getElementById("password");
 
 
 /*---------------------- *\
  * - Variables
 \* --------------------- */
 
-
+let x;
 
 /*---------------------- *\
  * - Fetch API
@@ -24,3 +28,27 @@
  * - Function
 \* --------------------- */
 
+logeout.addEventListener("click", () => {
+  window.location.href = "Login.html"
+})
+
+
+function run(e) {
+    if(username.value === "" || password.value === "") {
+    x = Math.floor(Math.random(1) * 44)
+    btn_login.classList.add(`translate-[${50}0%]`)
+    return
+  }
+
+
+  e.addEventListener("click" , () => {
+    window.location.href = "presence.html"
+  })
+
+} 
+
+
+  function focuss() {
+  btn_login.classList.toggle(`translate-[${50}0%]`)
+  // document.body.style.backgroundColor = "red"
+}
